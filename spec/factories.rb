9a01475e4f527f56d720f7c3493c78daf9296306
Parker Moore <parkrmoore@gmail.com>
2012-10-09 23:34:10 -0400

    Added factory for :response

---
+++ spec/factories.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define :response do |res|
+  res.ip_address = "192.168.1.1"
+  res.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.79 Safari/537.4"
+  res.who = "obama"
+end
