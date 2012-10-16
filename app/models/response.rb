91983c1e55df6c5b2c7b20c117e871bdac7cfb26
Parker Moore <parkrmoore@gmail.com>
2012-10-15 22:29:00 -0400

    Response: validation, accepting nested attrs for reasons

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,9 @@
 class Response < ActiveRecord::Base
   attr_accessible :ip_address, :user_agent, :who
   
+  validates_presence_of :ip_address, :user_agent, :who
+  validates_inclusion_of :who, :in => ['obama', 'romney', 'independent', 'didnt_vote']
+  
   has_many :reasons
+  accepts_nested_attributes_for :reasons
 end
