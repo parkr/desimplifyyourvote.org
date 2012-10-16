5698622be405547ea7a56c40f29e8db2ceac091c
Parker Moore <parkrmoore@gmail.com>
2012-10-15 22:29:36 -0400

    Redefined factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,11 @@
-FactoryGirl.define :response do |res|
-  res.ip_address = "192.168.1.1"
-  res.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.79 Safari/537.4"
-  res.who = "obama"
+FactoryGirl.define do
+  factory :response do
+    ip_address = "192.168.1.1"
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.79 Safari/537.4"
+    who = "obama"
+  end
+  factory :reason do
+    critique = 'WHATEVER MAN YOU SHOULD KNOW'
+    why = "It's all about love, man."
+  end
 end
