b0611dd34f470413c5fde3ed621f1e704f353048
Parker Moore <parkrmoore@gmail.com>
2012-10-16 00:58:18 -0400

    Added most configs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeSimplifyYourVote::Application.config.secret_token = 'bcb3361f21019cb745f39bdd794be13b0c3e228f40aadc4a15c7ed0c3226c1ef51dc99929db8ea1fb06658cc885c2cc9b257686405b9f164cb8a8921f18ecd2f'
