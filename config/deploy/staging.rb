65dc0e2ebbbf137410bb929ff926ca09067d5bf5
Parker Moore <parkrmoore@gmail.com>
2012-10-25 12:33:12 -0400

    Production & staging deploy configs

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,8 +3,11 @@ set :deploy_via, :remote_cache
 set :deploy_env, 'production'
 set :db_env, 'production'
 set :use_sudo, false
+set :server_name, "staging.desimplifyyourvote.org"
+set :sudo_user, "root"
 
 ssh_options[:username] = "root"
 ssh_options[:keys] = [File.join("","Users","parkermoore",".ssh","id_rsa")]
+default_run_options[:pty] = true
 
 role :app, '198.61.177.108'
