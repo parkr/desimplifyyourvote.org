2279771f48ad42a47b304393fd5856d0f36fb209
Parker Moore <parkrmoore@gmail.com>
2012-10-25 14:11:18 -0400

    don't need to log the reason

--- app/models/reason.rb
+++ app/models/reason.rb
@@ -2,7 +2,6 @@ class Reason < ActiveRecord::Base
   attr_accessible :critique, :response_id, :why
   
   validate do |reason|
-    logger.error("OH NOES: #{reason.inspect}")
     errors.add(:base, "Must include at least one of: critique, reason.") unless !(reason.critique.empty? && reason.why.empty?)
   end
   
